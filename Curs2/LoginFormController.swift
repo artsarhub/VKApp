2c2082df2e5c7e1035e9387fa379cf02a7244432
Artem Sarana <registrr2009@yandex.ru>
2020-12-06 15:18:31 +0300

    ДЗ к уроку 2

--- Curs2/LoginFormController.swift
+++ Curs2/LoginFormController.swift
@@ -8,11 +8,57 @@
 import UIKit
 
 class LoginFormController: UIViewController {
-
+    
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    private func checkUser() -> Bool {
+        guard let username = self.loginTF.text,
+              let password = self.passwordTF.text
+        else { return false }
+        return username == "1" && password == "1"
+    }
+    
+    private func showAlert() {
+        let alertController = UIAlertController(title: "Error",
+                                                message: "Неверные данные",
+                                                preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Close",
+                                        style: .cancel)
+        { _ in
+            self.loginTF.text = ""
+            self.passwordTF.text = ""
+        }
+        alertController.addAction(alertAction)
+        present(alertController,
+                animated: true,
+                completion: nil)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if !checkUser() {
+            showAlert()
+            return false
+        } else {
+            return true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        navigationController?.navigationBar.isHidden = false
     }
     
 
