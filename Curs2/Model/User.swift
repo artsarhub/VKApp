282b2491eb2b4b4b4e6a6a43754cacc519cb9a7c
Artem Sarana <registrr2009@yandex.ru>
2020-12-10 19:14:49 +0300

    Добавлена модель user в git

---
+++ Curs2/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Curs2
+//
+//  Created by Артём Сарана on 09.12.2020.
+//
+
+import UIKit
+
+class User {
+    var name: String
+    var avatar: UIImage
+    
+    init(_ name: String, _ avatar: UIImage) {
+        self.name = name
+        self.avatar = avatar
+    }
+}
